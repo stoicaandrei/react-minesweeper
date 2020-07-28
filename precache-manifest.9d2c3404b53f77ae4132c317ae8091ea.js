881ee0c4f9e39c5d27c2bce8f4e6ae1879605371
stoicaandrei <stoicaandrei@users.noreply.github.com>
2020-07-28 21:10:02 +0000

    deploy: 48ac1e7fd0451eeb0148bafc66b7f3c2b3c5f196

---
+++ precache-manifest.9d2c3404b53f77ae4132c317ae8091ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2b1b6709de698d13a6eb5ae3d4f3189",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c25676b2920979b0ba4",
+    "url": "/static/css/2.a72c35d3.chunk.css"
+  },
+  {
+    "revision": "5c25676b2920979b0ba4",
+    "url": "/static/js/2.1ac6d88e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1ac6d88e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53971462f12231fa52fd",
+    "url": "/static/js/main.19dc81aa.chunk.js"
+  },
+  {
+    "revision": "6113432b6e6aac3bd166",
+    "url": "/static/js/runtime-main.0ebac118.js"
+  }
+]);
